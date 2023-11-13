6bde6ca5687262ee980c98a1332dfca93288b6c8
DaurenKuantayev <52776105+DaurenKuantayev@users.noreply.github.com>
2023-11-13 19:15:30 +0600

    Add files via upload

---
+++ labWork2.py
@@ -0,0 +1,72 @@
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# numbers1 = []
+# for i in numbers:
+#     numbers1.append(i**2)
+#
+# print(numbers1)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers1 = [i**2 for i in numbers]
+#
+#
+# print(numbers1)
+# numbers1 = []
+# for i in numbers:
+#     if i % 2 == 0:
+#         numbers1.append(i)
+# numbers1 = [i for i in numbers if i % 2 == 0]
+# # numbers1 = [number for number in numbers if number % 2 == 0]
+# print(numbers1)
+#
+# numbers = 10 if numbers[-1] == 10 else 5
+# print(numbers1[-1])
+# names = ['Anar', 'Andrey', 'Mark', 'Ali']
+# greetings = []
+# for name in names:
+#     greeting = f'Hello, {name}'
+#     greetings.append(greetings)
+# print(greetings)
+#
+# greetings = [f'Hello, {name}' for name in names]
+# print(greetings)
+
+# age = 25
+# string = f'Hello, I am {age}'
+# print(string)
+#
+# name = 'Sanyia'
+# string1 = 'My name is {}, {}'.format('Saniya', "29")
+# print(string1)
+
+# text = 'Пример текста для преобразования'
+# t = []
+# for i in text:
+#     t.append(i.upper())
+# print(t)
+#
+# t = [i.upper() for i in text]
+# print(t)
+
+# from typing import List
+# a: int = 1
+# b: str = 'sanya'
+#
+# numbers: List[int] = [1, 2, 3]
+
+# def sum_numbers(a: int, b: int) -> int:
+#     return a + b
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers1 = [i*2 for i in numbers]
+# print(numbers1)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers1 = [i**2 for i in numbers if i % 2 == 0]
+print(numbers1)
+
+string =
+
+
+
+
